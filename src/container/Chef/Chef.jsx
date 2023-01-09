e12daa975dd35f0fb9a2c81c3f019b4066ee9e7e
sahuswastik <100347441+sahuswastik@users.noreply.github.com>
2023-01-09 12:44:15 +0530

    first commit

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Chef.css";
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem Ipsum Dolor Sit Amet, Consectetur Elit Auctor Sit.
+          </p>
+        </div>
+        <p className="p__opensans">
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
